ed8d878df7533e1fa5a6c3c56d90f6f9b88b4648
Kshitiz Rawat <kshitiz.crusader@gmail.com>
2018-07-09 14:31:16 +0530

    lets see

---
+++ app/src/main/java/com/mecamidi/www/mecamidiattendance/MainActivity.java
@@ -0,0 +1,22 @@
+package com.mecamidi.www.mecamidiattendance;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        //startActivity(new Intent(MainActivity.this, WelcomeActivity.class));
+
+
+    }
+}
